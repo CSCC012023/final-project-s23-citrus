b14923111c6b2ef204746b03b9322f5186903453
Daniel <dggrishanov007@gmail.com>
2023-06-16 17:10:51 -0400

    fix: fix user deletion, update singe user get to prisma

--- citrus/app/api/users/[id]/route.ts
+++ citrus/app/api/users/[id]/route.ts
@@ -1,11 +1,43 @@
 import * as db from '../../../../lib/db'
 import { NextResponse } from 'next/server';
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 // Retrieve a paginated list of all users in the database
-export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url);
-    const id = searchParams.get('id');
-    const res = await db.query("SELECT * FROM _temp WHERE username = $1", [id]);
-    const user = res.rows;
-    return NextResponse.json(user);
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    const username = params.id;
+
+    let select = {
+        username: true,
+        email: true,
+        premium: true,
+        experiences: true,
+    };
+
+    var user = await prisma.users.findUnique({
+        where: { username: username },
+        select: select
+    });
+    
+    return NextResponse.json( user );
+}
+
+// Endpoint to delete a user
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+    const username = params.id;
+
+    try {
+        await prisma.users.delete({
+            where: { username: username }
+        });
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === 'P2001') {
+                return NextResponse.json({ error: "record not found" }, { status: 404 });
+            }
+        }
+    }
+    return NextResponse.json({ username: username, message: "SUCCESS" });
+
 }
