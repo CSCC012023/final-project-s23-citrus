9817326240e256b02c5930dfbc51d6b48a3cd36b
Ishaan <ishaanupadhyay1004@gmail.com>
2023-06-30 14:24:39 -0400

    Initial version of event-specific details page

---
+++ citrus/app/experiences/[id]/page.tsx
@@ -0,0 +1,16 @@
+async function getEventData(id: string) {
+    const res = await fetch(`http://localhost:3000/api/experiences/${id}`);
+    const data = await res.json();
+    return data;
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+    const data = await getEventData(params.id);
+    console.log(data);
+    return (
+        <div>
+            <h1>{data.event_name}</h1>
+            <p>{data.event_description}</p>
+        </div>
+    );
+}
