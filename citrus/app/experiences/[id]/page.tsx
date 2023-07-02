b1a32185f2439482d169918e496a01589671bc0d
Ishaan <ishaanupadhyay1004@gmail.com>
2023-07-01 22:17:33 -0400

    Add header, footer, FontAwesome icons, polish page

--- citrus/app/experiences/[id]/page.tsx
+++ citrus/app/experiences/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendar, faLocationDot, faUser } from '@fortawesome/free-solid-svg-icons'
+
 async function getEventData(id: string) {
     const res = await fetch(`http://localhost:3000/api/experiences/${id}`);
     const data = await res.json();
@@ -10,18 +13,14 @@ export default async function Page({ params }: { params: { id: string } }) {
     const end_time = new Date(data.event_end);
     const map_url = `https://www.google.com/maps/embed/v1/place?key=${process.env.GOOGLE_MAPS_API_KEY}&q=${data.event_location}`;
     return (
-        <div>
+        <div className="w-9/12 m-auto">
             <h1 className="text-3xl">{data.event_name}</h1>
             <div id="tags" className="flex">
-                <p className="flex-none bg-cyan-500 rounded-lg border-cyan-200 border-2 px-4 mx-2 text-center">{data.category}</p>
+                <p className="flex-none bg-cyan-500 rounded-lg border-cyan-200 border-2 px-4 mr-2 text-center">{data.category}</p>
                 {data.tags.map((tag: string) => (
                     <p key={tag} className="flex-none bg-violet-900 rounded-lg border-violet-400 border-2 px-4 mx-2 text-center">{tag}</p>
                 ))}
             </div>
-            <p>Event dates: {start_time.toLocaleString()} to {end_time.toLocaleString()}</p>
-            <p>Description: {data.event_description}</p>
-            <p>Event location: {data.event_location}</p>
-            <p>Capacity: {data.capacity}</p>
             <iframe id="map"
                 className="
                 w-full
@@ -33,6 +32,19 @@ export default async function Page({ params }: { params: { id: string } }) {
                 referrerPolicy="no-referrer-when-downgrade"
                 src={map_url}>
             </iframe>
+            <div>
+                <h2 className="text-2xl">Details</h2>
+                <FontAwesomeIcon icon={faCalendar} className='text-3xl' />
+                <p>{start_time.toLocaleString()} to {end_time.toLocaleString()}</p>
+                <FontAwesomeIcon icon={faLocationDot} className='text-3xl' />
+                <p>Event location: {data.event_location}</p>
+                <FontAwesomeIcon icon={faUser} className='text-3xl' />
+                <p>Spots left: {data.capacity - data.attendees.length}</p>
+            </div>
+            <div>
+                <h2 className="text-2xl">Description</h2>
+                <p>{data.event_description}</p>
+            </div>
         </div>
     );
 }
--- citrus/app/layout.tsx
+++ citrus/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import { Inter } from 'next/font/google'
-
+import Image from 'next/image'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,7 +16,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={'bg-black h-screen flex flex-col'}>
+        <header className='flex h-min'>
+          <Image
+            src='/eventual_logo.svg'
+            alt='Citrus Logo'
+            width={200}
+            height={200}
+          />
+          <nav className='ml-auto my-auto mr-10'>
+            <ul className='flex text-xl space-x-10'>
+              <li className='flex-1'>
+                <a href="/">Home</a>
+              </li>
+              <li className='flex-1'>
+                <a href="/experiences">Experiences</a>
+              </li>
+              <li className='flex-1'>
+                <a href="/about">About</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <div className='flex-1'>
+          {children}
+        </div>
+        <footer className='flex justify-center space-x-4 bg-sky-700
+          text-xl'>
+          <p>Test</p>
+        </footer>
+      </body>
     </html>
   )
 }
