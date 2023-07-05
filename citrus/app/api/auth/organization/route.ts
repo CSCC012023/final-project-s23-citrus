9b27f9bdf61b5abad0400173157783e9b163ee5e
Ishaan Upadhyay <33141569+ishaan-upadhyay@users.noreply.github.com>
2023-07-05 16:28:54 -0400

    Events API Cleanup + Changes to make Next Auth work (#20)
    * Documentation cleanup, add API doc regenerate hook
    
    * Fix API routes to match new client
    
    * Create organization auth endpoint
    
    Co-authored-by: mirajismail <mirajismail@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: mirajismail <mirajismail@users.noreply.github.com>

---
+++ citrus/app/api/auth/organization/route.ts
@@ -0,0 +1,57 @@
+import * as db from '../../../../lib/db'
+import { NextResponse } from 'next/server';
+import { PrismaClient, Prisma } from '@prisma/client'
+import '../../../../lib/patch'
+
+const bcrypt = require('bcrypt');
+
+const prisma = new PrismaClient();
+
+/**
+ * @api {post} /api/auth/organization Login a user
+ * @apiName LoginOrganization
+ * @apiGroup Authentication
+ * 
+ * @apiBody {String} username The username of the organization
+ * @apiBody {String} password The password of the organization
+ * 
+ * @apiSuccess {String} username The display name of the organization
+ * @apiSuccess {String} email The email of the organization
+ * 
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 200 OK
+ *   {
+ *      "username": "sampleUsername",
+ *      "email": "test@email.com"
+ *   }
+ * 
+ * 
+ */
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    console.log("AUTHENTICATING USER...")
+
+    const email = body.email;
+    const password = body.password;
+
+    const organization = await prisma.organizers.findUnique({
+        where: { email: email }
+    });
+
+    if (!organization) {
+        return NextResponse.next();
+    }
+
+    const match = await bcrypt.compare(password, organization.pass);
+
+    if (!match) {
+        return NextResponse.next();
+    }
+
+    return NextResponse.json({
+        name: organization.display_name,
+        email: organization.email,
+
+    });
+}
