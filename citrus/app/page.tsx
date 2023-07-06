e876c9b599b40ef648a275b3b7024f37efd7ba57
Daniel <dggrishanov007@gmail.com>
2023-07-05 22:01:48 -0400

    CIT-40: add orginization API endpoints

--- citrus/app/page.tsx
+++ citrus/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-2 lg:text-left">
- 
+
         <a
           href="/login"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
@@ -54,7 +54,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Create a new account 
+            Create a new account
           </p>
         </a>
       </div>
