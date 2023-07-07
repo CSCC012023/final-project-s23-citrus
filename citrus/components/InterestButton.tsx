5e2cb9f963b83205ec1887e1add1392f55f80a65
PDOracle <114012137+PDOracle@users.noreply.github.com>
2023-07-07 00:19:15 -0400

    Implementing Component Refresh

--- citrus/components/InterestButton.tsx
+++ citrus/components/InterestButton.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useRouter } from "next/router";
 
 async function onInterestHandler(username: string | null | undefined, eventID: string){
     const res = await fetch(`/api/experiences/${eventID}`, {
@@ -7,12 +7,21 @@ async function onInterestHandler(username: string | null | undefined, eventID: s
         body: JSON.stringify({"id" : eventID, "attendees" : [username] }),
     });
     // Figure out how to make the page reload, or make this component reload
+    const router = useRouter();
+    router.replace(router.asPath); 
 }
 
 export default function InterestButton({username, eventID}: {username: string | null | undefined, eventID: string}){
     
     return (
-        <button onClick={async () => await onInterestHandler(username, eventID)}>I'm interested</button>
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="i h-16 w-64 bg-gradient-to-br from-yellow-400 to-yellow-600 items-center 
+            rounded-full shadow-2xl cursor-pointer absolute overflow-hidden transform hover:scale-x-110 
+            hover:scale-y-105 transition duration-300 ease-out">
+            </div>
+                <button className="text-center text-white font-semibold z-10 pointer-events-none" 
+                onClick={async () => await onInterestHandler(username, eventID)}>I'm interested</button>
+        </div>
     )
 
 }
