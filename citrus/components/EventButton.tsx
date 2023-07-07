0340c5ecb291071d9abb128ef479670486a3ae5a
Ishaan Upadhyay <33141569+ishaan-upadhyay@users.noreply.github.com>
2023-07-07 17:36:03 -0400

    [CIT-41] Allow users to express interest in events (#28)
    * Co-authored-by: Ishaan Upadhyay <ishaan-upadhyay@users.noreply.github.com>
    Co-authored-by: TheDannyG <TheDannyG@users.noreply.github.com>
    Co-authored-by: mirajismail <mirajismail@users.noreply.github.com>
    
    * Implement CIT-41 (Expressing User Interest)
    
    * Implementing Component Refresh
    
    * Implement Page Refresh(for real this time)
    
    * Cleanup
    
    ---------
    
    Co-authored-by: PDOracle <114012137+PDOracle@users.noreply.github.com>

---
+++ citrus/components/EventButton.tsx
@@ -0,0 +1,30 @@
+import { getServerSession } from 'next-auth/next'
+import type { Session } from 'next-auth';
+import { use } from "react";
+import InterestButton from '@/components/InterestButton'
+
+
+async function eventInfo(eventID: string) {
+    const res = await fetch(process.env.BASE_API_URL + `api/experiences/${eventID}`);
+    const data = await res.json();
+    return data;
+}
+
+export default async function EventButton({eventID}: {eventID: string}){
+    
+    const session = await getServerSession();
+    if(!session){
+        return <p>You&apos;re not signed in.</p>
+    }
+
+    const data = await eventInfo(eventID);
+
+    if(data.attendees.includes(session.user?.name)) {
+        return <p>You&apos;re already interested in this event.</p>
+    }
+
+    return (
+        <InterestButton eventID={eventID} username={session.user?.name}/>
+    )
+
+}
