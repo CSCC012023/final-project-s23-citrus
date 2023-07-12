022bf692c62d067084f33fa9a5a94771a1967dc2
Ishaan <ishaanupadhyay1004@gmail.com>
2023-07-12 15:10:45 -0400

    CIT-35: bug fix (missing closing tags)

--- citrus/app/(organizers)/organizer/layout.tsx
+++ citrus/app/(organizers)/organizer/layout.tsx
@@ -20,7 +20,7 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   const session = await getServerSession(authOptions);
-  const correctUserType = (!session) || (session.user && session.user.userType != 'user');
+  const correctUserType = (!session) || (session.user && session.user.userType !== 'user');
 
   return (
     <html lang="en">
@@ -40,6 +40,7 @@ export default async function RootLayout({
               </li>
               <li className='flex-1'>
                 <a href="/organizer/create">Create</a>
+              </li>
               <li>
                 <a href="organizer/dashboard">Dashboard</a>
               </li>
