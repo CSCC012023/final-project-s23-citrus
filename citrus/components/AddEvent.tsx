f0b87609392b1cc23c76b744634bb8e0bf43d2a0
Ishaan <ishaanupadhyay1004@gmail.com>
2023-07-21 12:11:33 -0400

    CIT-44: Add page for users to create events

--- citrus/components/AddEvent.tsx
+++ citrus/components/AddEvent.tsx
@@ -3,6 +3,7 @@
 import { useState, FormEvent, ChangeEvent} from "react";
 import styles from "@/app/(organizers)/organizer/create/create.module.css";
 import { useSession, signIn, signOut } from 'next-auth/react'
+import { usePathname } from "next/navigation";
 
 import {
   Flex,
@@ -12,7 +13,9 @@ import {
   Stack,
   Box,
   FormControl,
+  Textarea,
 } from "@chakra-ui/react";
+
 
 export default function AddEvent() {
   const [name, setName] = useState("");
@@ -25,12 +28,13 @@ export default function AddEvent() {
   const [tags, setTags] = useState("");
 
   const { data: session } = useSession();
+  const path = usePathname();
 
   const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
   };
 
-  const handleDescriptionChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleDescriptionChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setDescription(event.target.value);
   };
 
@@ -60,30 +64,31 @@ export default function AddEvent() {
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+    const organizer_fields = {
+      "org_id": path.includes("organizer") ? session?.user?.name : null,
+      "user_id": path.includes("organizer") ? null : session?.user?.name
+    }
     // Handle form submission here
     const res = await fetch('/api/experiences', {
         method: 'POST',
         body: JSON.stringify({"name" : name, "description": description, "capacity": parseInt(capacity), "location": location,
-          "start_time": start, "end_time": end, "category": category, "tags": tags.split(","), "org_id": session?.user?.name }),
+          "start_time": start, "end_time": end, "category": category, "tags": tags.split(","), ...organizer_fields }),
     });
   };
 
-  if (!session || session.user?.userType != "organizer") {
+  if (!session) {
     return (<div>
-              <p>You must be logged in as an organizer to create an event:</p>
+              <p>You must be logged in to create an event.</p>
               <li>
-                <a href={'/organizer/login'}>Login</a>
+                <a href={path.includes("organizer") ? "/organizer/login": "/login"}>Login</a>
               </li>
             </div>
     )
   } else {
     return (
       <Flex
-      className={styles["custom-background"]}
+      className={styles["custom-background"] + " h-full w-full"}
         flexDirection="column"
-        width="100vw"
-        height="100vh"
         justifyContent="center"
         alignItems="center"
       >
@@ -92,44 +97,45 @@ export default function AddEvent() {
         p="1rem"
         backgroundColor="whiteAlpha.900"
         boxShadow="md"
-        className={styles["custom-box"]}
+        className={styles["custom-box"] + " w-9/12"}
         >
           <Heading className={styles["custom-heading"]} textAlign="center">
             Create Event
           </Heading>
-          <Box minW={{ base: "90%", md: "468px" }}>
-            <form onSubmit={handleSubmit}>
+          <Box className="w-full">
+            <form onSubmit={handleSubmit} className="w-full">
               <Stack
-                className={styles["custom-avatar-container"]} // Apply the custom CSS class
+                className="w-full justify-items-center flex"
                 flexDir="column"
                 mb="2"
                 justifyContent="center"
                 alignItems="center"
               >
-
-                <FormControl>
+                <h2>Event name</h2>
+                <FormControl className="w-8/12 mx-auto">
                   <Input
-                    type="name"
+                    type="text"
                     placeholder="Event Name"
                     value={name}
                     onChange={handleNameChange}
-                    className={styles["custom-input"]}
+                    className={styles["custom-input"] + " w-full"}
                   />
                 </FormControl>
 
-                <FormControl>
-                  <Input
-                    type="description"
+                <h2>Event description</h2>
+                <FormControl className="w-9/12 mx-auto">
+                  <Textarea
                     placeholder="Event Description"
                     value={description}
                     onChange={handleDescriptionChange}
-                    className={styles["custom-input"]}
+                    className={styles["custom-input"] + " w-full resize-y"}
                   />
                 </FormControl>
 
+                <h2>Event capacity</h2>
                 <FormControl>
                   <Input
-                    type="capacity"
+                    type="number"
                     placeholder="Event Capacity"
                     value={capacity}
                     onChange={handleCapacityChange}
@@ -137,19 +143,21 @@ export default function AddEvent() {
                   />
                 </FormControl>
 
-                <FormControl>
+                <h2>Event location</h2>
+                <FormControl className="w-7/12 mx-auto">
                   <Input
                     type="location"
                     placeholder="Event Location"
                     value={location}
                     onChange={handleLocationChange}
-                    className={styles["custom-input"]}
+                    className={styles["custom-input"] + " w-full"}
                   />
                 </FormControl>
 
+                <h2>Event start and end time</h2>
                 <FormControl>
                   <Input
-                    type="start"
+                    type="datetime-local"
                     placeholder="Event Start Time"
                     value={start}
                     onChange={handleStartChange}
@@ -159,7 +167,7 @@ export default function AddEvent() {
 
                 <FormControl>
                   <Input
-                    type="end"
+                    type="datetime-local"
                     placeholder="Event End Time"
                     value={end}
                     onChange={handleEndChange}
@@ -167,6 +175,7 @@ export default function AddEvent() {
                   />
                 </FormControl>
 
+                <h2>Event category</h2>
                 <FormControl>
                   <Input
                     type="category"
@@ -177,6 +186,7 @@ export default function AddEvent() {
                   />
                 </FormControl>
 
+                <h2>Event tags (must be comma-separated)</h2>
                 <FormControl>
                   <Input
                     type="tags"
