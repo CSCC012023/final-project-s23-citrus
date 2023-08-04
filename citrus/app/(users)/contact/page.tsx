ea35bfec35e157d91d1590e64d321cccc33d1eeb
Ishaan <ishaanupadhyay1004@gmail.com>
2023-08-04 15:09:47 -0400

    [CIT-31] Fix unescaped chars

--- citrus/app/(users)/contact/page.tsx
+++ citrus/app/(users)/contact/page.tsx
@@ -11,12 +11,12 @@ const AboutUsPage: React.FC = () => {
             Welcome to Eventual - your gateway to meaningful connections and exciting events! 
           </p>
           <p className={styles.description}>
-            Our platform allows you to connect with like-minded individuals, attend events, and share your own gatherings with the community. Whether it's discovering new hobbies, exploring your city's hidden gems, or simply making new friends, Eventual is here to make it happen.
+            Our platform allows you to connect with like-minded individuals, attend events, and share your own gatherings with the community. Whether it&apos;s discovering new hobbies, exploring your city&apos;s hidden gems, or simply making new friends, Eventual is here to make it happen.
           </p>
           <p className={styles.description}>
             We take pride in providing exceptional tech support, available 24/7, to ensure your experience with us is seamless and enjoyable. Our team is always ready to assist, so you can focus on what matters most - connecting, engaging, and having a great time!
           </p>
-          <p className={styles.phoneNumber}>Join us today and become a part of our thriving community. Let's create lasting memories and build meaningful connections together!</p>
+          <p className={styles.phoneNumber}>Join us today and become a part of our thriving community. Let&apos;s create lasting memories and build meaningful connections together!</p>
           <p className={styles.phoneNumber}>-</p>
 
           <p className={styles.phoneNumber}>Phone: 647-324-8322</p>
